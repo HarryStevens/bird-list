c70f241f6486d72b691d0b63738239d0c96bd899
HarryStevens <harryjosephstevens@gmail.com>
2014-04-15 15:52:47 -0400

    Layouts and dialogs in process

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
 /**
  * @author Harry Stevens
  */
+$(document).ready(function() {
+	clickHandler();
+});
+
+function clickHandler() {
+	$(".bird").on("click", function() {
+		console.log("clickeroo!");
+		$(".data").dialog({
+			show : {
+				effect : "fold",
+				duration : 1000
+			}
+		}, {
+			height : 560
+		}, {
+			width : 794
+		}, {
+			position : "center"
+		}, {
+			dialogClass : "data-alert"
+		}, {
+			hide : {
+				effect : "fold",
+				duration : 1000
+			}
+		});
+	});
+}
