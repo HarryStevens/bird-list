fffe21642a9451f5d7c2b08d5c857f71edf45c84
HarryStevens <harryjosephstevens@gmail.com>
2014-04-16 18:29:57 -0400

    Buttons depress and first data

--- scripts.js
+++ scripts.js
@@ -1,19 +1,68 @@
 /**
  * @author Harry Stevens
  */
+
+//When everything is loaded, this function will luanch clickHandler
 $(document).ready(function() {
+	pageLoaded();
 	clickHandler();
 	//draggable();
 });
 
-function draggable() {
-	$(".bird").draggable();
+//pageLoaded function loads the Google Visualization library and calls the clickHandler
+function pageLoaded() {
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		callback : googleLoaded
+	});
 }
 
+// Load the json from the fusion table and call dataLoaded function
+function googleLoaded() {
+	//Get request grabs the data from Google Fusion Table
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1lygsoHUDr_8FzACROCbqWSG5Lh09d2VVYaQKav2T&key=AIzaSyB-QJux9WIJmey5IJYzPImNzg-xP1gpvU8", dataLoaded, "json");
+}
+
+function dataLoaded(BIRDS) {
+	var data = google.visualization.arrayToDataTable([
+		["Year","Herring Gull", "American Kestrel"],
+		["1990",5,2],
+		["1991",7,2],
+		["1992",8,2]
+		]);
+	var options = {
+		backgroundColor : {
+			//stroke : '#000',
+			//strokeWidth : 1,
+			fill: '#fdf9f3',
+		},
+		legend : {
+			//position : 'top',
+		},
+		height : 450,
+		width: 760,
+		chartArea : {
+			//width: '80%',
+			//height: '80%'
+		},
+		vAxis : {
+			title : 'Number',
+		},
+		hAxis : {
+			title : 'Year',
+		},
+		colors: ['#50a6c2','#af593d']
+	};
+	var chart = new google.visualization.ColumnChart(document.getElementById('data'));
+	chart.draw(data, options);
+}
+
+//This function explains what to do what a bird card is clicked (i.e. any element with class "bird")
 function clickHandler() {
 	$(".bird").on("click", function() {
+		//dialog is a jQuery UI method. It launches a dialog box, and there are lots of options for it. Its CSS is controlled by classes
+		//that begin with .ui-
 		$(".data").dialog({
-
 			show : {
 				effect : "drop",
 				duration : 500
@@ -29,12 +78,39 @@ function clickHandler() {
 		}, {
 			position : "center"
 		}, {
-			dialogClass : "data-alert"
-		}, {
 			hide : {
 				effect : "drop",
 				duration : 500
 			}
 		});
+		//End dialog options
 	});
+	//end jQuery UI click
+
+	//This will make the bird cards depress slightly when users click the mouse, and return to the original state when they release the mouse
+	$(".bird").on("mousedown", function() {
+		var currID = $(this).attr('id');
+		$("#" + currID).addClass("depress");
+	});
+	$(".bird").on("mouseup", function() {
+		var currID = $(this).attr('id');
+		$("#" + currID).removeClass("depress");
+	});
+}//end ClickHandler
+
+//This is a joke function to drag all the bird cards around. It is disabled in the document ready function,
+//but can be activated by removing the slahes in front of it. It's hilarious.
+function draggable() {
+	$(".bird").draggable();
 }
+
+/*
+ //Puts the right image in place
+ function imager () {
+ var parentID = $(".bird-photo").parent().attr("id");
+ console.log(parentID);
+ $(".bird-photo").html('<img src="images/'+parentID+'.jpg" />')
+ console.log('<img src="images/'+parentID+'.jpg" />');
+ }
+ */
+
